ac857e49fdb0505fe3f7f2e9dbe2f270e38b9f16
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-15 22:51:55 -0400

    adding the main search function for the giphy searcher. also setting up and getting rid of the bloated react app.

--- design-api-gify/src/App.js
+++ design-api-gify/src/App.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 class App extends Component {
+  handleTermChange(term) {
+    console.log(term);
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <SearchBar onTermChange={this.handleTermChange} />
       </div>
     );
   }
 }
 
 export default App;
+
+//1st layer after root-----------------------------------------
+// this is called the render method
