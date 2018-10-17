93c9696c447ad303e3a4308d611ce59395fe7e48
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-16 23:36:18 -0400

    took out the service worker. The simplest of things

--- design-api-gify/src/index.js
+++ design-api-gify/src/index.js
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import GifList from './components/GifList';
+import SearchBar from './components/SearchBar';
+import GifModal from './components/GifModal';
+import request from 'superagent';
+import './styles/app.css';
+
+class App extends Component {
+  //constructor for the gifs
+  constructor(){
+    super();
+
+    this.state = {
+      gifs: [],
+      selectedGif: null,
+      modalIsOpen: false
+    };
+  }
+
+  openModal(gif){
+    this.setState({
+      modalIsOpen: true,
+      selectedGif: gif
+    });
+  }
+
+  closeModal(){
+    this.setState({
+      modalIsOpen: false,
+      selectedGif: null
+    })
+  }
+
+  handleTermChange = (term) => {
+    const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=T8B0aFWsJ5E6edMuilem7xYMuDgNwdhA`;
+
+    request.get(url, (err, res) => {
+      this.setState({ gifs: res.body.data})
+      //console.log(res.body.data[0]);
+    });
+    //console.log(term);
+  }
+  //main render method
+  render() {
+    return (
+      <div className="App">
+        <SearchBar onTermChange={this.handleTermChange} />
+        <GifList gifs={this.state.gifs} 
+                 onGifSelect={selectedGif => this.openModal(selectedGif) }/>
+        <GifModal modalIsOpen={this.state.modalIsOpen}
+                selectedGif={this.state.selectedGif}
+                onRequestClose={ () => this.closeModal()} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-
-///ROOOOOOOOT--------------------------------------------------------------------------
+//1st layer after root-----------------------------------------
+// this is called the render method
